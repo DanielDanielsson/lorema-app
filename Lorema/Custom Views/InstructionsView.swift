f774bc3690d122b2b13f9ca105f53a66902195b1
DanielDanielsson <danielssondaniel94@gmail.com>
2026-02-07 07:37:36 +0100

    feat: add custom views for instructions and permissions

---
+++ Lorema/Custom Views/InstructionsView.swift
@@ -0,0 +1,49 @@
+//
+//  NoPermissionView.swift
+//  Lorema
+//
+//  Created by Daniel Danielsson on 2022-04-29.
+//
+
+import Cocoa
+
+class InstructionsView: NSView, LoadableView {
+
+// MARK: - IBOutlet Properties
+    @IBOutlet weak var instructionLoremText: NSTextField!
+    @IBOutlet weak var loremPrefix: NSTextField!
+    @IBOutlet weak var loremWordSpace: NSTextField!
+    @IBOutlet weak var loraWordSpace: NSTextField!
+    @IBOutlet weak var instructionLoraText: NSTextField!
+    @IBOutlet weak var loraPrefix: NSTextField!
+    
+// MARK: - Init
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        
+        _ = load(fromNIBNamed: "InstructionsView")
+        
+        loremPrefix.stringValue = (UserDefaults.standard.value(forKey: "loPrefix") as? String)!
+        loraPrefix.stringValue = (UserDefaults.standard.value(forKey: "laPrefix") as? String)!
+        loremWordSpace.stringValue = "+ number of words + space"
+        loraWordSpace.stringValue = "+ number of words + space"
+        instructionLoremText.stringValue = "Text starting with \"Lorem ipsum\", type:"
+        instructionLoraText.stringValue = "Random text, type:"
+    
+        NotificationCenter.default.addObserver(self, selector: #selector(handleDidChangePrefix(notification:)), name: NSNotification.Name(rawValue: "didChangePrefixes"), object: nil)
+    }
+    
+    @objc func handleDidChangePrefix(notification: Notification) {
+        if let preferredLorem = UserDefaults.standard.value(forKey: "loPrefix") as? String {
+            loremPrefix.stringValue = preferredLorem
+        }
+        
+        if let preferredLora = UserDefaults.standard.value(forKey: "laPrefix") as? String {
+            loraPrefix.stringValue = preferredLora
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
---
+++ Lorema/Custom Views/NoPermissionView.swift
@@ -0,0 +1,31 @@
+//
+//  NoPermissionView.swift
+//  Lorema
+//
+//  Created by Daniel Danielsson on 2022-04-30.
+//
+
+import Cocoa
+
+class NoPermissionView: NSView, LoadableView {
+    
+    @IBOutlet weak var instructionText: NSTextField!
+    @IBOutlet weak var settingsPathText: NSTextField!
+    
+    @IBAction func goToSettings(_ sender: Any) {
+        NSWorkspace.shared.open(URL(string: "x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility")!)
+    }
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        _ = load(fromNIBNamed: "NoPermissionView")
+
+        instructionText.stringValue = "In order for Lorema to run, you need to allow accessability permission in the security & privacy settings."
+        settingsPathText.stringValue = "System Preferences > Security & Privacy > Accessibility, then add and checkmark Lorema."
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
